65b4c24cdf6b81c8f5c33c2bb1986c07c75179a3
Bridget <bridget4light@gmail.com>
2023-03-15 08:26:00 -0700

    my static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _atoi - convert a string into an integer.
  *
@@ -6,11 +7,11 @@
  *
  * Return: integer.
  */
+
 int _atoi(char *s)
 {
 	int sign = 1, i = 0;
 	unsigned int res = 0;
-
 
 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
@@ -18,7 +19,7 @@ int _atoi(char *s)
 			sign *= -1;
 		i++;
 	}
-	while ([i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
 		res = (res * 10) + (s[i] - '0');
 		i++;
